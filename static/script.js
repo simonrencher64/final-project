0fe56c9849b55a92ef1a141f949ee74aed86b0d4
OwenABC1 <owennsullivan@icloud.com>
2025-01-08 13:07:11 -0800

    first 10 buttons display values

--- static/script.js
+++ static/script.js
@@ -1,6 +1,17 @@
 
 
 $(document).ready(function(){
+	$.get('/get_collection_data', function(data) {
+		for(var i=0; i < 100; i++){
+			$("#"+i).children().text(data[i-1]);
+		}
+	});
+	
+	
+	
+	
+	
+	
   $("button").click(function(){
     $.post("/button_press",
     {
