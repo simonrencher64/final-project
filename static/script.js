653f9d343f15950ead50efa427ddd946b4001bab
Your NameE-Perko <edward.p.perko@gmail.com>
2025-01-13 14:15:55 -0800

    colors

--- static/script.js
+++ static/script.js
@@ -30,7 +30,36 @@ $(document).ready(function(){
 			
 			var value = Number($(this).children().text());
 			value += 1;
+			if(value >= 10 && value < 25){
+				$(this).css('background-color', '#ADD8E6')
+			}
+			else if(value >= 25 && value < 50){
+				$(this).css('background-color', '#87CEFA')
+			}
+			else if(value >= 50 && value < 75){
+				$(this).css('background-color', '#6495ED ')
+			}
+			else if(value >= 75 && value < 100){
+				$(this).css('background-color', '#4169E1')
+			}
+			else if(value >= 100 && value < 200){
+				$(this).css('background-color', '#0000FF')
+			}
+			else if(value >= 200 && value < 300){
+				$(this).css('background-color', '#0000CD')
+			}
+			else if(value >= 300 && value < 400){
+				$(this).css('background-color', '#00008B')
+			}
+			else if(value >= 400 && value < 500){
+				$(this).css('background-color', '#000080')
+			}
+			else if(value >= 500){
+				$(this).css('background-color', '#191970')
+			}
+			else{$(this).css('background-color', '#5F9EA0 ')}
 			$(this).children().text(value);
+			
 		}
 	});
 	
