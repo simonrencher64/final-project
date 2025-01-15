1ed26dbaea4705a84aae7df189a8cf24e98dc586
Your NameE-Perko <edward.p.perko@gmail.com>
2025-01-15 12:58:34 -0800

    error message

--- static/script.js
+++ static/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 			$("#"+i).css('background-color', 'rgb(' + rgbdata[0] + "," + rgbdata[1] + "," + rgbdata[2] + ")");
 		}
 	});
-	
+	var logged_in = false;
 	$.ajax({
 		type: 'GET',
 		url: "/check_loggin",
@@ -26,7 +26,10 @@ $(document).ready(function(){
 		success: function(data) {
 			if(data["logged_in"] == true){
 				$(".button").addClass("clickable");
+				logged_in = true;
+				$("#error").hide();
 			}
+			
 		}
 	});
 	
@@ -41,57 +44,63 @@ $(document).ready(function(){
 	});
 	
 	$('.button').click(function(){
-		if($(this).hasClass('clickable')) {
-			$.ajax({
-				type: 'POST',
-				url: "/button_press",
-				data: { number: $(this).attr('id') },
-				success: function(data) {
-					
+		if(logged_in == true){
+			if($(this).hasClass('clickable')) {
+				$.ajax({
+					type: 'POST',
+					url: "/button_press",
+					data: { number: $(this).attr('id') },
+					success: function(data) {
+						
+					}
+
+				});
+				
+				var value = Number($(this).children().text());
+				value += 1;
+				$(this).children().text(value);
+				
+				var rgbdata = get_color(value);
+				$(this).css('background-color', 'rgb(' + rgbdata[0] + "," + rgbdata[1] + "," + rgbdata[2] + ")");
+				
+				
+				
+				/*
+				if(value >= 10 && value < 25){
+					$(this).css('background-color', '#ADD8E6')
 				}
-			});
-			
-			var value = Number($(this).children().text());
-			value += 1;
-			$(this).children().text(value);
-			
-			var rgbdata = get_color(value);
-			$(this).css('background-color', 'rgb(' + rgbdata[0] + "," + rgbdata[1] + "," + rgbdata[2] + ")");
-			
-			
-			
-			/*
-			if(value >= 10 && value < 25){
-				$(this).css('background-color', '#ADD8E6')
+				else if(value >= 25 && value < 50){
+					$(this).css('background-color', '#87CEFA')
+				}
+				else if(value >= 50 && value < 75){
+					$(this).css('background-color', '#6495ED ')
+				}
+				else if(value >= 75 && value < 100){
+					$(this).css('background-color', '#4169E1')
+				}
+				else if(value >= 100 && value < 200){
+					$(this).css('background-color', '#0000FF')
+				}
+				else if(value >= 200 && value < 300){
+					$(this).css('background-color', '#0000CD')
+				}
+				else if(value >= 300 && value < 400){
+					$(this).css('background-color', '#00008B')
+				}
+				else if(value >= 400 && value < 500){
+					$(this).css('background-color', '#000080')
+				}
+				else if(value >= 500){
+					$(this).css('background-color', '#191970')
+				}
+				else{$(this).css('background-color', '#5F9EA0 ')}
+				*/
+				
+				
 			}
-			else if(value >= 25 && value < 50){
-				$(this).css('background-color', '#87CEFA')
-			}
-			else if(value >= 50 && value < 75){
-				$(this).css('background-color', '#6495ED ')
-			}
-			else if(value >= 75 && value < 100){
-				$(this).css('background-color', '#4169E1')
-			}
-			else if(value >= 100 && value < 200){
-				$(this).css('background-color', '#0000FF')
-			}
-			else if(value >= 200 && value < 300){
-				$(this).css('background-color', '#0000CD')
-			}
-			else if(value >= 300 && value < 400){
-				$(this).css('background-color', '#00008B')
-			}
-			else if(value >= 400 && value < 500){
-				$(this).css('background-color', '#000080')
-			}
-			else if(value >= 500){
-				$(this).css('background-color', '#191970')
-			}
-			else{$(this).css('background-color', '#5F9EA0 ')}
-			*/
-			
-			
+	    }
+		else{
+				$("#error").show();
 		}
 	});
 	
